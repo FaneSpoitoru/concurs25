e72e7f52e58e7bde051500a90bda24e0603e2afe
antidate723 <lapinfoo@gmail.com>
2025-05-05 20:43:48 +0300

    minore

--- index.js
+++ index.js
@@ -83,3 +83,45 @@ document.addEventListener("DOMContentLoaded", function () {
         });
       }
       
+
+      document.addEventListener('DOMContentLoaded', () => {
+        const toggleButton = document.getElementById('darkModeToggle');
+      
+        if (!toggleButton) return;
+      
+        // Aplică tema dark dacă era salvată anterior
+        if (localStorage.getItem('theme') === 'dark') {
+          document.body.classList.add('dark-mode');
+        }
+      
+        toggleButton.addEventListener('click', () => {
+          document.body.classList.toggle('dark-mode');
+      
+          if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('theme', 'dark');
+          } else {
+            localStorage.setItem('theme', 'light');
+          }
+        });
+      });
+      
+
+      function googleTranslateElementInit() {
+        new google.translate.TranslateElement({
+            pageLanguage: 'ro',
+            includedLanguages: 'ro,en,de,it,fr,es',
+            layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+        }, 'google_translate_element');
+    }
+
+
+
+
+        setTimeout(() => {
+          const el = document.getElementById("text");
+          el.remove(); // sau el.remove();
+        }, 3500);
+
+      
+
+        document.getElementById("year").textContent = new Date().getFullYear();
